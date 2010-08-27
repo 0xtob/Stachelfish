0296810bb4fc76af0a154d23cda1b1d80354c827
Arne Schmitz <arne.schmitz@gmx.net>
2010-08-27 18:24:03 +0200

    - add SDL opengl stub

---
+++ opengl.c
@@ -0,0 +1,40 @@
+#include <SDL/SDL.h>
+#include <SDL/SDL_opengl.h>
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    if( SDL_Init( SDL_INIT_VIDEO ) != 0 )
+    {
+        printf("failed to init SDL.\n");
+        return 1;
+    }
+
+    printf("success.\n");
+    
+    SDL_SetVideoMode( 800, 450, 32, SDL_OPENGL );
+    
+    while (1)
+    {
+        SDL_Event event;
+
+        while( SDL_PollEvent(&event) )
+        {
+            switch(event.type)
+            {
+            case SDL_QUIT:
+                SDL_Quit();
+                exit(0);
+                break;
+            default:
+                break;
+            }
+        }
+
+    }
+
+    SDL_Quit();
+    
+    return 0;
+}
