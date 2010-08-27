df9cf491fa40d4c48dc2b1c1955ed996dc67537c
Arne Schmitz <arne.schmitz@gmx.net>
2010-08-27 20:44:25 +0200

    - support comments :)

--- convert-shader.py
+++ convert-shader.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
+import re
+
 def convertShader(name, in_filename, out_filename):
     shader_in = open(in_filename, "r")
     shader_out = open(out_filename, "w");
     shader_out.write("const char *%s = \"" % (name))
     for line in shader_in:
-        shader_out.write(line.strip())
+        shader_out.write(re.sub("//.*", "", line.strip()))
         shader_out.write("\\\n")
     shader_out.write("\";\n")
     shader_in.close()
