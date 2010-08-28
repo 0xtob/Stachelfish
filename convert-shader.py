9b8aedc36a9a1574e89dc3729b0b5c3c32c6ae97
Arne Schmitz <arne.schmitz@gmx.net>
2010-08-28 11:26:57 +0200

    - some other way of storing the shader

--- convert-shader.py
+++ convert-shader.py
@@ -5,13 +5,14 @@ import re
 def convertShader(name, in_filename, out_filename):
     shader_in = open(in_filename, "r")
     shader_out = open(out_filename, "w");
-    shader_out.write("const char *%s = \"" % (name))
+    shader_out.write("const char *%s = " % (name))
     for line in shader_in:
         line = re.sub("//.*", "", line).strip()
+        line = re.sub("\"", "\\\"", line).strip()
         if len(line) > 0:
-            shader_out.write(line)
-            shader_out.write("\\\n")
-    shader_out.write("\";\n")
+            shader_out.write("\"%s\\n\"" % (line))
+            shader_out.write("\n")
+    shader_out.write(";\n")
     shader_in.close()
     shader_out.close()
 
